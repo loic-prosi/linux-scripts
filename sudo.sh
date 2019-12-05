debdef58a37f1f78df458d02db9f2fb7fdaa5519
Loïc Prosi <8113779+loic-prosi@users.noreply.github.com>
2019-12-05 02:30:49 +0100

    Update sudo.sh

--- sudo.sh
+++ sudo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install sudo and add one user in the sudo group
-# Launch the script in root with this command: ./sudo.sh username
+# Launch this script in root with this command: ./sudo.sh username
 
 USER=$1
 
