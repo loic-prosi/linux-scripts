5201b1504d4a9a988029917bc2b065c1b379f6ff
Loïc Prosi <8113779+loic-prosi@users.noreply.github.com>
2019-12-06 18:14:14 +0100

    Update vscode.sh

--- vscode.sh
+++ vscode.sh
@@ -7,3 +7,9 @@ install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
 sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 apt-get update
 apt-get install code
+
+# Install extensions
+
+code --install-extension --force Compulim.vscode-clock file-icons.file-icons whtouche.vscode-js-console-utils dbaeumer.vscode-eslint esbenp.prettier-vscode fabiospampinato.vscode-monokai-night
+humao.rest-client msjsdiag.debugger-for-chrome ms-vsliveshare.vsliveshare-pack
+
