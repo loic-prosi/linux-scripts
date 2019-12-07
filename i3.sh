b0b9c903ad9e85347134a313b5938ff3fc5c5c38
Loïc Prosi <8113779+loic-prosi@users.noreply.github.com>
2019-12-07 23:18:38 +0100

    Update i3.sh

--- i3.sh
+++ i3.sh
@@ -3,11 +3,6 @@
 # Install i3
 apt install xorg i3
 
-# Change the default keyboard layout in french
-setxkbmap fr
-rm .config/i3/config
-i3-config-wizard
-
 # Make i3 start each time the user log in
 echo "" >> .profile
 echo "#Startx Automatically" >> .profile
