92213977a09750b1576390839bbb48b2de39295b
Loïc Prosi <8113779+loic-prosi@users.noreply.github.com>
2019-12-08 00:33:22 +0100

    Update google-chrome.sh

--- google-chrome.sh
+++ google-chrome.sh
@@ -2,6 +2,7 @@
 
 # Add Google Chrome repository to sources.list then install it
 
+sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt update
 sudo apt install google-chrome-stable
