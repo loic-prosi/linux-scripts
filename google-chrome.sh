e85e365f31580f37f529f18d5567ababec09154f
Loïc Prosi <8113779+loic-prosi@users.noreply.github.com>
2019-12-08 00:26:55 +0100

    Added google-chrome.sh

---
+++ google-chrome.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Google Chrome repository to sources.list then install it
+
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt update
+sudo apt install google-chrome-stable
