3705faad76cae6473a0dacc7fb2e0e793974f810
Loïc Prosi <8113779+loic-prosi@users.noreply.github.com>
2019-12-07 23:43:08 +0100

    Update i3.sh

--- i3.sh
+++ i3.sh
@@ -4,11 +4,6 @@
 apt install xorg i3
 
 # Make i3 start each time the user log in
-echo "" >> .profile
-echo "#Startx Automatically" >> .profile
-echo "if [[ -z "$DISPLAY" ]] && [[ $(tty) = /dev/tty1 ]]; then" >> .profile
-echo " . startx" >> .profile
-echo " logout" >> .profile
-echo "fi" >> .profile
+echo "startx" >> .profile
 
 reboot
