745f73ba92eef67c8ca5b9b8a69134676782ae82
Loïc Prosi <8113779+loic-prosi@users.noreply.github.com>
2019-12-08 01:29:54 +0100

    Update vb_guests_additions.sh

--- vb_guests_additions.sh
+++ vb_guests_additions.sh
@@ -8,6 +8,6 @@ MOUNT_PATH=$2
 
 wget download.virtualbox.org/virtualbox/$VERSION_VIRTUALBOX/VBoxGuestAdditions_$VERSION_VIRTUALBOX.iso
 sudo mount VBoxGuestAdditions_$VERSION_VIRTUALBOX.iso $MOUNT_PATH
-$MOUNT_PATH/VBoxLinuxAdditions.run
+sudo $MOUNT_PATH/VBoxLinuxAdditions.run
 rm VBoxGuestAdditions_$VERSION_VIRTUALBOX.iso
 sudo reboot
