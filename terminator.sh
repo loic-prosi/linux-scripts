8904945e2d5b111cba5e7df984dbe813961069d3
Loïc Prosi <8113779+loic-prosi@users.noreply.github.com>
2019-12-08 02:15:41 +0100

    Update terminator.sh

--- terminator.sh
+++ terminator.sh
@@ -3,3 +3,4 @@
 # Install terminator and add custom config file to /.config/terminator/config
 
 sudo apt install terminator
+cp $HOME/linux-scripts/config-files/terminator $HOME/.config/terminator/config
