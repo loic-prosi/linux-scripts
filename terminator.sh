ec4bca3b0d4a9f53abacce32b2bf6ca45b79beff
Loïc Prosi <8113779+loic-prosi@users.noreply.github.com>
2019-12-08 07:00:52 +0100

    Delete terminator.sh

--- terminator.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Install terminator and add custom config file to /.config/terminator/config
-
-sudo apt install terminator
-cp $HOME/linux-scripts/config-files/terminator $HOME/.config/terminator/config
