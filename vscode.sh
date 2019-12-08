c10b8fca93216548e4c4b06372d78d3d9d6e9438
Loïc Prosi <8113779+loic-prosi@users.noreply.github.com>
2019-12-08 06:21:34 +0100

    Update vscode.sh

--- vscode.sh
+++ vscode.sh
@@ -3,8 +3,8 @@
 # Add Visual Studio Code repository to sources.list then install it
 
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
-sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get update
 sudo apt-get install code
 
