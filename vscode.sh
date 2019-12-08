e608bae431b02159dd6b2586e129318138f14ec2
Loïc Prosi <8113779+loic-prosi@users.noreply.github.com>
2019-12-08 21:47:26 +0100

    Update vscode.sh

--- vscode.sh
+++ vscode.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Add Visual Studio Code repository to sources.list then install it
-
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
@@ -9,7 +8,6 @@ sudo apt-get update
 sudo apt-get install code
 
 # Install extensions
-
 code --install-extension Compulim.vscode-clock
 code --install-extension file-icons.file-icons
 code --install-extension fabiospampinato.vscode-monokai-night
@@ -19,3 +17,6 @@ code --install-extension esbenp.prettier-vscode
 code --install-extension humao.rest-client
 code --install-extension msjsdiag.debugger-for-chrome
 code --install-extension ms-vsliveshare.vsliveshare-pack
+
+# Launch Visual Studio Code
+code
