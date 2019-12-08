4b6784a0727c2b0326a4ca05da3f6ad5cc69efcc
Loïc Prosi <8113779+loic-prosi@users.noreply.github.com>
2019-12-08 18:19:06 +0100

    Update vb_guests_additions.sh

--- vb_guests_additions.sh
+++ vb_guests_additions.sh
@@ -6,6 +6,7 @@
 VERSION_VIRTUALBOX=$1
 MOUNT_PATH=$2
 
+sudo apt install linux-headers-`uname -r`
 wget download.virtualbox.org/virtualbox/$VERSION_VIRTUALBOX/VBoxGuestAdditions_$VERSION_VIRTUALBOX.iso
 sudo mount VBoxGuestAdditions_$VERSION_VIRTUALBOX.iso $MOUNT_PATH
 sudo $MOUNT_PATH/VBoxLinuxAdditions.run
