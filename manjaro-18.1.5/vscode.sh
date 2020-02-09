eced59894cc8870916bbd4e2e1dfc65eba568adf
Loïc Prosi <8113779+loic-prosi@users.noreply.github.com>
2020-02-09 21:01:00 +0100

    Create vscode.sh

---
+++ manjaro-18.1.5/vscode.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo pamac build visual-studio-code-bin
+
+# Install extensions
+code --install-extension Compulim.vscode-clock
+code --install-extension file-icons.file-icons
+code --install-extension fabiospampinato.vscode-monokai-night
+code --install-extension whtouche.vscode-js-console-utils
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension esbenp.prettier-vscode
+code --install-extension humao.rest-client
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension ms-vsliveshare.vsliveshare-pack
