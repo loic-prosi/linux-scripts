2a9bde970be90484c47377881c2eea1644116c4b
Loïc Prosi <8113779+loic-prosi@users.noreply.github.com>
2020-07-20 03:42:59 +0200

    Update zsh.sh

--- debian-10.2.0/zsh.sh
+++ debian-10.2.0/zsh.sh
@@ -2,7 +2,7 @@
 
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-#Changing the shell with chsh command
+#You can changing the default shell with the chsh command
 #Enter root password then type /bin/bash
 
 #Changing the default shell to zsh can cause the auto starting commands in the .profile file not working anymore
